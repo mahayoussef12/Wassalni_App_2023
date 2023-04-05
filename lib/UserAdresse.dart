1b593d7490647c79d48ff23e607ec7fae47adf92
Maha Youssef <91993267+mahayoussef53@users.noreply.github.com>
2023-04-05 10:51:01 +0100

    finally_commit

--- lib/UserAdresse.dart
+++ lib/UserAdresse.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -8,14 +7,12 @@ import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'Firebase/AuthentificationController.dart';
-
 import 'Session_User/Controller.dart';
 import 'package:custom_info_window/custom_info_window.dart';
-
 import 'Session_User/TaxiReservationWidget.dart';
+import 'package:url_launcher/url_launcher.dart';
 class CurrentLocationScreenUser extends StatefulWidget {
 
   const CurrentLocationScreenUser ({Key? key, }) : super(key: key);
@@ -197,96 +194,118 @@ class _CurrentLocationScreenState extends State<CurrentLocationScreenUser> {
     );
   }
   Set<Marker> getmarkers() {
-      for (var i in controller_Adresse.userData) {
-        setState(() {
-          _markers.add(
-              Marker(
-                  markerId: MarkerId("${i.name}"),
-                  position: LatLng(i.latitude, i.longitude),
-                  //icon: markerbitmap,
-                  onTap: () {
-                    _customInfoWindowController.addInfoWindow!(
-                      Container(
-                        width: 300,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 300,
-                              height: 80,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                    image: NetworkImage(
-                                        'https://img.freepik.com/photos-gratuite/voiture-taxi-lumieres-bokeh_181624-26108.jpg?w=740&t=st=1680129497~exp=1680130097~hmac=ac477f7b90d7b86a8e3b0e8f8a26969f809fdbc2f5f8435e084e5aded5466f39'),
-                                    fit: BoxFit.fitWidth,
-                                    filterQuality: FilterQuality.high),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10.0),
-                                ),
-                                color: Colors.red,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 10, left: 10, right: 10),
-                              child: Row(
-                                children: [
-                                  SizedBox(
-                                    width: 100,
-                                    child: Text(
-                                      '${i.name}',
-                                      maxLines: 1,
-                                      overflow: TextOverflow.fade,
-                                      softWrap: false,
-                                    ),
-                                  ),
-                                  const Spacer(),
-                                  TextButton(onPressed: () {
-                                    Get.to(CompleteForm());
-                                  }, child: Text("reservation")
-                                    ,
-                                    // widget.data!.date!,
-
-                                  )
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  top: 10, left: 10, right: 10),
-                              child: Center(child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  IconButton(
-                                      onPressed: () {}, icon: Icon(Icons.phone)),
-                                  IconButton(
-                                      onPressed: () {}, icon: Icon(Icons.message))
-                                ],
-                              )
-                              ),
-
-                            )
-                          ],
-                        ),
+    for (var i in controller_Adresse.userData) {
+      setState(() {
+        _markers.add(
+            Marker(
+                markerId: MarkerId("${i.name}"),
+                position: LatLng(i.latitude, i.longitude),
+                //icon: markerbitmap,
+                onTap: () {
+                  _customInfoWindowController.addInfoWindow!(
+                    Container(
+                      width: 300,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      LatLng(i.latitude, i.longitude),
-                    );
-                  }
-              ));
-        });
-      }
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 300,
+                            height: 80,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      'https://img.freepik.com/photos-gratuite/voiture-taxi-lumieres-bokeh_181624-26108.jpg?w=740&t=st=1680129497~exp=1680130097~hmac=ac477f7b90d7b86a8e3b0e8f8a26969f809fdbc2f5f8435e084e5aded5466f39'),
+                                  fit: BoxFit.fitWidth,
+                                  filterQuality: FilterQuality.high),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10.0),
+                              ),
+                              color: Colors.red,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10, left: 10, right: 10),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: 100,
+                                  child: Text(
+                                    '${i.name}',
+                                    maxLines: 1,
+                                    overflow: TextOverflow.fade,
+                                    softWrap: false,
+                                  ),
+                                ),
+                                const Spacer(),
+                                TextButton(onPressed: () {
+                                  Get.to(CompleteForm());
+                                }, child: Text("reservation")
+                                  ,
+                                  // widget.data!.date!,
 
-      //add more markers here
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10, left: 10, right: 10),
+                            child: Center(child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(
+                                    onPressed: () async {
+                                      final Uri url = Uri(
+                                        scheme:'tel',
+                                        path: '${i.number}',
+                                      );
+                                      if (await canLaunchUrl(url)){
+                                        await launchUrl(url);
+                                      }else {
+                                        print("cannot launch this url");
+                                      }
+                                    }, icon: Icon(Icons.phone)),
+                                IconButton(
+                                    onPressed: () async {
+                                      final Uri  launchUri = Uri(
+                                        scheme:'sms',
+                                        path: '${i.number}',
+                                      );
+                                      if (await canLaunch(launchUri.toString())){
+                                        await launch (launchUri.toString());
+                                      }else {
+                                        print("the action is not supported (No Phone App)");
+                                      }
+
+                                    }, icon: Icon(Icons.message))
+                              ],
+                            )
+                            ),
+
+                          )
+                        ],
+                      ),
+                    ),
+                    LatLng(i.latitude, i.longitude),
+                  );
+                }
+            ));
+      });
+    }
+
+    //add more markers here
 
 
     return _markers;
   }
+
 
 }
